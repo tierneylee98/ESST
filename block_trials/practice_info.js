1044a43963aec98fb0bcd7e950c6bf3aa14a632c
tierneylee98 <31345893+tierneylee98@users.noreply.github.com>
2021-01-13 13:57:49 +0800

    few edits
    corrected block label from all practice to block1 etc.
    
    edited practice feedback duration to 500ms.
    
    changed css background and text color
    
    added extra practice trial

--- block_trials/practice_info.js
+++ block_trials/practice_info.js
@@ -38,5 +38,45 @@ var practice_info = [
     "arrow_key_correct": "n",
     "arrow_image": "signals/right_stim.bmp",
     "trial_response_correct": "x"
+  },
+  {
+    "emotion_type": "neg",
+    "emotion_image": "IAPS/neg4.bmp",
+    "emotion_condition": "neg4",
+    "condition": "go",
+    "arrow_direction": "left",
+    "arrow_key_correct": "c",
+    "arrow_image": "signals/left_stim.bmp",
+    "trial_response_correct": "l"
+  },
+  {
+    "emotion_type": "neg",
+    "emotion_image": "IAPS/neg2_m.bmp",
+    "emotion_condition": "neg2_m",
+    "condition": "go",
+    "arrow_direction": "right",
+    "arrow_key_correct": "n",
+    "arrow_image": "signals/right_stim.bmp",
+    "trial_response_correct": "r"
+  },
+  {
+    "emotion_type": "neu",
+    "emotion_image": "IAPS/neutral9_m.bmp",
+    "emotion_condition": "neutral9_m",
+    "condition": "stop",
+    "arrow_direction": "left",
+    "arrow_key_correct": "c",
+    "arrow_image": "signals/left_stim.bmp",
+    "trial_response_correct": "x"
+  },
+  {
+    "emotion_type": "neu",
+    "emotion_image": "IAPS/neutral27_m.bmp",
+    "emotion_condition": "neutral27_m",
+    "condition": "stop",
+    "arrow_direction": "right",
+    "arrow_key_correct": "n",
+    "arrow_image": "signals/right_stim.bmp",
+    "trial_response_correct": "x"
   }
 ];
